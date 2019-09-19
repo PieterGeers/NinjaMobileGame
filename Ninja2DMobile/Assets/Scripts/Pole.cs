75d75e4da3d3782e88d4cd592f52846b45682cc3
PieterGeers <48602601+PieterGeers@users.noreply.github.com>
2019-09-19 11:20:57 +0300

    random level gen

---
+++ Ninja2DMobile/Assets/Scripts/Pole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pole : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0.5f;
+
+
+    void Update()
+    {
+        transform.position += Vector3.left * _speed * Time.deltaTime;
+    }
+}
---
+++ Ninja2DMobile/Assets/Scripts/PoleManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleManager : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxTime = 1.0f;
+    private float _timer = 0.0f;
+    [SerializeField]
+    private float _height = 1.0f;
+    [SerializeField]
+    private GameObject _poleReference = null;
+
+
+    private void Start()
+    {
+        if (_poleReference == null)
+            throw new System.Exception("_poleReference = NULL");
+    }
+
+    private void Update()
+    {
+        if (_timer >= _maxTime)
+        {
+            GameObject newPole = Instantiate(_poleReference);
+            newPole.transform.position = transform.position + new Vector3(0.0f, Random.Range(_height, -_height), 0.0f);
+            Destroy(newPole, 10);
+
+            _timer = 0.0f;
+        }
+
+        _timer += Time.deltaTime;
+    }
+}
