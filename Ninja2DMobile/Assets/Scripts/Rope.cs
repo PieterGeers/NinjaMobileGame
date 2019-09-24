94934736e04ce3ce0592ea185d0c351ca876e33a
PieterGeers <48602601+PieterGeers@users.noreply.github.com>
2019-09-24 21:47:22 +0300

    Initial rope physics

---
+++ Ninja2DMobile/Assets/Scripts/Rope.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _hook = null;
+    [SerializeField]
+    private GameObject _link = null;
+    [SerializeField]
+    private uint _links = 7;
+
+    private void Awake()
+    {
+        if (_hook == null)
+            throw new System.Exception("_hook = NULL");
+        if (_link == null)
+            throw new System.Exception("_link = NULL");
+    }
+
+    private void Start()
+    {
+        GenerateRope();
+    }
+
+
+    private void GenerateRope()
+    {
+        Rigidbody2D previousRB = _hook;
+        for (uint i = 0; i < _links; ++i)
+        {
+            GameObject newLink = Instantiate(_link, transform);
+            HingeJoint2D joint = newLink.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRB;
+
+            previousRB = newLink.GetComponent<Rigidbody2D>();
+        }
+    }
+}
+
