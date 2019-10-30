fb847fc4ecc9f9a73a13d8ee1109c4f9ac80fe58
Cem Alagozlu <cem.alagozlu@hotmail.com>
2019-10-30 14:19:49 +0200

    ui menu start

---
+++ Ninja2DMobile/Assets/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class Menu : MonoBehaviour
+{
+    public Color newColor;
+    private float _timer;
+    private float _counterSpeed;
+
+
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer < 1.5f) 
+        {
+            GetComponentInChildren<TMP_Text>().alpha -= Time.deltaTime;
+        }
+        else if (_timer > 2.5f && _timer < 4.5f)
+        {
+            GetComponentInChildren<TMP_Text>().alpha += Time.deltaTime;
+        }
+        else if (_timer > 4.5f)
+        {
+            _timer = 0.0f;
+        }
+    }
+}
