765b18a021f164ffb5c7e399ec053389ab2db10c
Cem Alagozlu <cem.alagozlu@hotmail.com>
2019-10-30 19:28:21 +0200

    U.I. menu & ingame improved

--- Ninja2DMobile/Assets/Scripts/GameState.cs
+++ Ninja2DMobile/Assets/Scripts/GameState.cs
@@ -10,4 +10,19 @@ public class GameState : MonoBehaviour
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(0);
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void ReturnHome()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
--- Ninja2DMobile/Assets/Scripts/Menu.cs
+++ Ninja2DMobile/Assets/Scripts/Menu.cs
@@ -1,35 +1,44 @@
 ﻿using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class Menu : MonoBehaviour
 {
-    public Color newColor;
-    private float _timer;
-    private float _counterSpeed;
-
+    //Fader Text 'Tap to start'
+    private bool _isFaded;
+    private float _alpha;
+    [SerializeField] private TMP_Text _text;
 
 
     void Start()
     {
+        _isFaded = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        _timer += Time.deltaTime;
+        TextFader();
+    }
 
-        if (_timer < 1.5f) 
-        {
-            GetComponentInChildren<TMP_Text>().alpha -= Time.deltaTime;
-        }
-        else if (_timer > 2.5f && _timer < 4.5f)
-        {
-            GetComponentInChildren<TMP_Text>().alpha += Time.deltaTime;
-        }
-        else if (_timer > 4.5f)
-        {
-            _timer = 0.0f;
-        }
+
+    void TextFader()
+    {
+        //fades in/out the text
+        if (_isFaded)  { _alpha += Time.deltaTime; }
+        else if (!_isFaded) { _alpha -= Time.deltaTime; }
+
+        //Check if text can fade or not
+        if (_alpha >= 1.0f) { _isFaded = false; }
+        else if (_alpha <= 0.0f) { _isFaded = true; }
+
+        //changes the component
+          _text.alpha = _alpha;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Landscape");
     }
 }
