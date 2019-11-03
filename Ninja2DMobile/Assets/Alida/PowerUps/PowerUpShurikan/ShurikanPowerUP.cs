9b2451ab61f80414ea576e926caf0e5dead57734
Stoarm <Alidahalma@hotmail.com>
2019-11-03 12:02:22 +0200

    Slowmotion PowerUp

---
+++ Ninja2DMobile/Assets/Alida/PowerUps/PowerUpShurikan/ShurikanPowerUP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikanPowerUP : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D Player1)
+    {
+        if (Player1.CompareTag("Player"))
+        {
+            Pickup(Player1);
+        }
+
+
+    }
+
+    void Pickup(Collider2D player)
+    {
+       Debug.Log("Picked Up.");
+
+        //empty
+
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Ninja2DMobile/Assets/Alida/PowerUps/PowerUpSlowmo/Slowmotion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slowmotion : MonoBehaviour
+{
+
+    private float duration = 4;
+
+    void OnTriggerEnter2D(Collider2D Player1)
+    {
+        if (Player1.CompareTag("Player"))
+        {
+           StartCoroutine(Pickup(Player1));
+        }
+    }
+
+    IEnumerator Pickup(Collider2D player)
+    {
+
+        Player speed = player.GetComponent<Player>();
+        Time.timeScale = 0.5f;
+        Time.fixedDeltaTime = 0.02F * Time.timeScale;
+
+        GetComponent<PolygonCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = 0.02F;
+
+        Destroy(gameObject);
+    }
+
+}
