f171e64d18632f49cdf4ffbb5fe59862f535a314
Stoarm <Alidahalma@hotmail.com>
2019-11-18 20:48:15 +0200

    Revert "PowerUP Shuriken"
    This reverts commit 1c1f65ffaababa8dcb8ac343e05f30100fc3a0d9.

--- Ninja2DMobile/Assets/Alida/PowerUps/PowerUpShurikan/ShurikanPowerUP.cs
+++ Ninja2DMobile/Assets/Alida/PowerUps/PowerUpShurikan/ShurikanPowerUP.cs
@@ -4,38 +4,25 @@ using UnityEngine;
 
 public class ShurikanPowerUP : MonoBehaviour
 {
-    private float duration = 4;
-    public GameObject shuriken;
-    private GameObject[] gos;
-    private int array = 10;
 
     void OnTriggerEnter2D(Collider2D Player1)
     {
         if (Player1.CompareTag("Player"))
         {
-            StartCoroutine(Pickup(Player1));
-
+            Pickup(Player1);
         }
+
+
     }
 
-    IEnumerator Pickup(Collider2D player)
+    void Pickup(Collider2D player)
     {
+       Debug.Log("Picked Up.");
 
-        shuriken = GameObject.FindWithTag("Shuriken");
-
-        GetComponent<PolygonCollider2D>().enabled = false;
-        GetComponent<SpriteRenderer>().enabled = false;
-
-        gos = new GameObject[array];
-        for (int i = 0; i < array; i++)
-        {
-            GameObject clone = Instantiate(shuriken, new Vector3((float)i, 1, 0), Quaternion.identity) as GameObject;
-            clone.transform.localScale = Vector3.one;
-            gos[i] = clone;
-        }
-
-        yield return new WaitForSeconds(duration);
+        //empty
 
         Destroy(gameObject);
     }
+
+
 }
